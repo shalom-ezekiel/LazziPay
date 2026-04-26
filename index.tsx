682858259cbefd9bbf697920abed0248a4a5cac5
Shalom Ekekiel <107555717+showzycode@users.noreply.github.com>
2026-04-26 02:37:35 +0100

    feat: wrap App in PartnerProvider context

--- index.tsx
+++ index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
+import { PartnerProvider } from './contexts/PartnerContext';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -15,7 +16,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <PartnerProvider>
+          <App />
+        </PartnerProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
